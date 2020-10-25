b62bf470eba4166de222731b93751a9cff23b3fa
kimhaun <wha02068@gmail.com>
2020-10-25 16:03:51 +0900

    입력한 데이터를 들고 결과를 설정해서 메인으로 복귀

--- app/src/main/java/kr/co/neoplus/intent_20201025/EditDataActivity.kt
+++ app/src/main/java/kr/co/neoplus/intent_20201025/EditDataActivity.kt
@@ -1,13 +1,27 @@
 package kr.co.neoplus.intent_20201025
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_data.*
 
 class EditDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_data)
 
+        okBtn.setOnClickListener {
+            val inputData = newDataEdt.text.toString()
 
+            //출발지, 목적지 x 데이터만 옮기는용
+            val resultIntent = Intent()
+            resultIntent.putExtra("newData", inputData)
+
+            //확인 누른 거 맞아 resultIntent도 가지고 돌아가라.
+            setResult(Activity.RESULT_OK, resultIntent)
+
+            finish()
+        }
     }
 }
