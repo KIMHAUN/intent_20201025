f3a4fb045e66beed8e6cca5ef7020c8bff2a5581
kimhaun <wha02068@gmail.com>
2020-10-25 14:49:44 +0900

    finish()를 활용해서 이전 화면으로 복귀하기

--- app/src/main/java/kr/co/neoplus/intent_20201025/MyFirstActivity.kt
+++ app/src/main/java/kr/co/neoplus/intent_20201025/MyFirstActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.neoplus.intent_20201025
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_my_first.*
 
 class MyFirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_first)
+
+        backBtn.setOnClickListener {
+
+            //val myIntent = Intent(this, MainActivity::class.java);
+            //startActivity(myIntent)
+
+//            현재 화면을 종료하고 돌아가자.
+            finish()
+        }
     }
 }
