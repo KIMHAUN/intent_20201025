565263a5d43cbf85529401967fa0d529a68244c3
kimhaun <wha02068@gmail.com>
2020-10-25 16:13:39 +0900

    들고 돌아온 데이터를 받아서 반영. requestCode, resultCode 검사해서 상황이 맞을 때만 데이터를 꺼내서 처리

--- app/src/main/java/kr/co/neoplus/intent_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/intent_20201025/MainActivity.kt
@@ -1,11 +1,12 @@
 package kr.co.neoplus.intent_20201025
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class ty : AppCompatActivity() {
 
 //    클래스 정의부분 바로 밑에 적는 변수 : 멤버변수
 //    function 안에서 적는 변수 : 지역변수(로컬변수)
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     val REQ_FOR_DATA = 1000
 
 
+    //이 화면이 만들어질 때 실행되는 함수
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,10 +46,26 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, EditDataActivity::class.java)
 //            왕복티켓
             startActivityForResult(myIntent, REQ_FOR_DATA)
-
-
-
-
         }
     }
+
+    //결과를 받아올  때 이 함수를 실행함.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //데이터 가지러 갔다온 게 맞는지 확인
+        if(requestCode == REQ_FOR_DATA){
+
+            //확인 버튼 누른 게 맞니? (resultCode)
+            //data? (Intent?이기 때문에.)
+            if(resultCode == Activity.RESULT_OK){
+                val newInputData = data?.getStringExtra("newData")
+                dataTxt.text = newInputData
+            }
+
+        }
+
+
+    }
+
+
 }
